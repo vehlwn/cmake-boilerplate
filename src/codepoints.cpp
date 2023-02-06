e244740350e300987e0ef715f6ace59242f45766
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-02-06 18:55:41 +0400

    Add clang-format

--- src/codepoints.cpp
+++ src/codepoints.cpp
@@ -5,24 +5,27 @@
 
 #include <boost/regex/icu.hpp>
 
-int main(const int argc, const char *argv[]) {
-  if (argc == 1) {
-    std::cerr << "usage: " << argv[0] << " <string> " << std::endl;
-    std::exit(1);
-  }
+int main(const int argc, const char* argv[])
+{
+    if(argc == 1) {
+        std::cerr << "usage: " << argv[0] << " <string> " << std::endl;
+        std::exit(1);
+    }
 
-  const auto re = boost::make_u32regex(".");
-  const auto text = argv[1];
-  std::cout << "text = \'" << text << "\'" << '\n';
+    const auto re = boost::make_u32regex(".");
+    const auto text = argv[1];
+    std::cout << "text = \'" << text << "\'" << '\n';
 
-  using CharIterator = const char *;
-  using RegexIterator = boost::u32regex_iterator<CharIterator>;
-  const RegexIterator it = boost::make_u32regex_iterator(text, re);
-  std::for_each(
-      it, RegexIterator(), [](const boost::match_results<CharIterator> &m) {
-        const auto sub =
-            std::string_view(m[0].first, static_cast<std::size_t>(m.length()));
-        std::cout << "length = " << m.length() << ", sub = \'" << sub << "\'\n";
-      });
-  return 0;
+    using CharIterator = const char*;
+    using RegexIterator = boost::u32regex_iterator<CharIterator>;
+    const RegexIterator it = boost::make_u32regex_iterator(text, re);
+    std::for_each(
+        it,
+        RegexIterator(),
+        [](const boost::match_results<CharIterator>& m) {
+            const auto sub
+                = std::string_view(m[0].first, static_cast<std::size_t>(m.length()));
+            std::cout << "length = " << m.length() << ", sub = \'" << sub << "\'\n";
+        });
+    return 0;
 }
--- src/random.cpp
+++ src/random.cpp
@@ -4,11 +4,12 @@
 #include <boost/random/random_device.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
 
-int main() {
-  boost::random::random_device rd;
-  boost::random::mt19937_64 gen(rd());
-  boost::random::uniform_int_distribution<int> dist(1, 100);
-  for (int i = 0; i < 10; i++) {
-    std::cout << dist(gen) << std::endl;
-  }
+int main()
+{
+    boost::random::random_device rd;
+    boost::random::mt19937_64 gen(rd());
+    boost::random::uniform_int_distribution<int> dist(1, 100);
+    for(int i = 0; i < 10; i++) {
+        std::cout << dist(gen) << std::endl;
+    }
 }
