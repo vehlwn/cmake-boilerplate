ee542019740a45087c905df1be24b333ad1008c6
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-02-08 13:44:50 +0400

    Add icu::StringCharacterIterator example

--- src/bench-codepoints.cpp
+++ src/bench-codepoints.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <array>
+#include <chrono>
+#include <cstddef>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -7,6 +9,9 @@
 #include <string_view>
 
 #include <boost/regex/icu.hpp>
+#include <unicode/schriter.h>
+#include <unicode/umachine.h>
+#include <unicode/unistr.h>
 
 std::string copyToString(std::istream& is)
 {
@@ -35,7 +40,6 @@ int main(const int argc, const char* argv[])
     }
 
     std::string text;
-    const auto re = boost::make_u32regex(".");
     if(std::ifstream f{argv[1]}) {
         text = copyToString(f);
     } else {
@@ -45,9 +49,25 @@ int main(const int argc, const char* argv[])
 
     std::cout << "text.size = " << text.size() << std::endl;
 
+    auto t1 = std::chrono::system_clock::now();
+    const auto re = boost::make_u32regex(".");
     using CharIterator = std::string::const_iterator;
     using RegexIterator = boost::u32regex_iterator<CharIterator>;
     const auto it = boost::make_u32regex_iterator(text, re);
     std::cout << "distance = " << std::distance(it, RegexIterator()) << std::endl;
+    auto t2 = std::chrono::system_clock::now();
+    std::cout << std::chrono::duration<double>(t2 - t1).count() << " s" << std::endl;
+
+    t1 = std::chrono::system_clock::now();
+    icu::UnicodeString utext = text.c_str();
+    icu::StringCharacterIterator it2(utext);
+    std::size_t distance2 = 0;
+    for(UChar32 c = it2.first32(); c != icu::ForwardCharacterIterator::DONE;
+        c = it2.next32()) {
+        distance2++;
+    }
+    std::cout << "distance2 = " << distance2 << '\n';
+    t2 = std::chrono::system_clock::now();
+    std::cout << std::chrono::duration<double>(t2 - t1).count() << " s" << std::endl;
     return 0;
 }
