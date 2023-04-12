d4ae6b2300fdeab32ccddb3bdccd253bc5cb87d6
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-04-12 17:51:40 +0400

    Update conanfile.py to support conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,24 @@
-import conans
+from conan import ConanFile
+from conan.tools.gnu import PkgConfigDeps
+from conan.tools.meson import MesonToolchain, Meson
 
 
-class BoilerplateConan(conans.ConanFile):
-    generators = ["pkg_config", "cmake"]
-    settings = "os", "compiler", "build_type", "arch"
-    requires = ["boost/1.81.0", "icu/72.1"]
-    default_options = {"icu:data_packaging": "static"}
+class BoilerplateConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    default_options = {"icu/*:data_packaging": "static"}
+
+    def requirements(self):
+        self.requires("boost/1.81.0")
+        self.requires("icu/72.1")
+
+    def generate(self):
+        tc = MesonToolchain(self)
+        tc.project_options["use_conan"] = "true"
+        tc.generate()
+        pc = PkgConfigDeps(self)
+        pc.generate()
 
     def build(self):
-        meson = conans.Meson(self)
+        meson = Meson(self)
         meson.configure()
         meson.build()
